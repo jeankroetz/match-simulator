dba2879bc8fc6dd1dd6c21e2884c9100c5b3732d
jeankroetz <jekroetz@gmail.com>
2022-06-21 09:22:14 -0300

    first commit

---
+++ app/src/main/java/com/example/simuladordepartidas/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.example.simuladordepartidas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simuladordepartidas.databinding.ActivityDetailBinding
+import com.example.simuladordepartidas.databinding.ActivityMainBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+}
